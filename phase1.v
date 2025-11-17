74ae9aff28d89606b9dbb3fd5524ebc72efc8dd6
Allan Anderson <allanderson1128@gmail.com>
2025-11-17 14:46:26 -0800

    Hidden layer instantiation

--- phase1.v
+++ phase1.v
@@ -4,8 +4,43 @@ module phase1 #(
     input   wire                      clk,
     input   wire                      rst_n,    // Active-low reset
     input   wire [DATA_W*IN_DIM-1:0]  bus_in,
-    output  wire [9:0]                out,
+    input   wire                      start,
+    output  wire [9:0]                one_out,
 );
+
+// Container size for MAC operations and bias
+localparam ACC_W = 40;
+
+// Internal signals
+/*
+PROJECT NOTE:
+weights and bias can be converted to ports temporarily if we want to try out different values
+in the testbench to find the best. If so, just comment them out below and redeclare them as 
+wires instead of reg in the ports.
+*/
+reg [7:0] hidden_done;  // 8 node outputs
+reg [7:0][DATA_W-1:0] hidden_out;   // 8 done flags (ideally only ever 0x00 or 0xFF)
+reg [7:0][ACC_W-1:0] bias;  // 8 biases to be set
+reg [7:0][IN_DIM*DATA_W-1:0] weight;    // 8 weight vectors to be set
+
+// Instantiation of 8 node hidden layer
+genvar i;
+generate;
+    for (i = 0; i < 8; i++) begin : HIDDEN_LAYER
+        hidden_node #(.IN_DIM(IN_DIM), .DATA_W(DATA_W), .ACC_W(ACC_W))
+            node_i (
+                .clk(clk),
+                .rst_n(rst_n),
+                .start(start),
+                .in(bus_in),
+                .bias(bias[i]),
+                .weight(weight[i]),
+                .out(hidden_out[i]),
+                .done(hidden_done[i])
+            );
+    end
+endgenerate
+
     
 endmodule
 
@@ -22,8 +57,8 @@ module hidden_node #(
     input   wire [ACC_W-1:0]          bias,
     input   wire [DATA_W*IN_DIM-1:0]  weight,
     
-    output  reg [DATA_W-1:0]          out,
-    output  reg                       done
+    output  reg [DATA_W-1:0]          out,  // In this case, 8 bit to be input for out layer
+    output  reg                       done  // Flag for confirmation to start out layer
 );
 
 // FSM state definition
@@ -42,6 +77,7 @@ reg [2*DATA_W-1:0]       product;
 reg [ACC_W-1:0]          acc;
 
 always @(posedge clk or negedge rst_n) begin
+    // Active-Low reset
     if (!rst_n) begin
         state <= IDLE;
         acc <= 0;
@@ -49,6 +85,7 @@ always @(posedge clk or negedge rst_n) begin
         idx <= 0;
         done <= 1'b0;
     end else begin
+        // FSM logic
         state <= next_state;
 
         case(state)
@@ -72,7 +109,6 @@ always @(posedge clk or negedge rst_n) begin
 
             DONE: begin
                 done <= 1'b1;
-
                 //ReLU
                 if(acc[ACC_W-1] == 1'b1) begin
                     out <= 0;
@@ -86,6 +122,7 @@ always @(posedge clk or negedge rst_n) begin
     end
 end
 
+// Next state logic
 always @(*) begin
     next_state = state;
 
